ab96479153f472cfe4bef03f7e21877f2d7ef3cc
Zachary Ebenfeld <zackebenfeld@gmail.com>
2025-01-09 23:19:56 +1100

    refactoring

--- App.tsx
+++ App.tsx
@@ -1,28 +1,13 @@
 // @refresh reset
 import { useEffect } from "react";
-import { ScrollView, StyleProp, TextStyle } from "react-native";
+import { ScrollView } from "react-native";
 import Animated, {
-  AnimatedStyle,
   useAnimatedStyle,
   useSharedValue,
   withDelay,
   withRepeat,
   withSpring,
 } from "react-native-reanimated";
-
-function Container({ children }: { children: React.ReactNode }) {
-  return (
-    <ScrollView
-      contentContainerStyle={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {children}
-    </ScrollView>
-  );
-}
 
 export default function ReanimatedPlayground() {
   return (
@@ -32,86 +17,83 @@ export default function ReanimatedPlayground() {
   );
 }
 
-function AnimatedLetter({ text, index }: { text: string; index: number }) {
-  const fromY = 1;
-  const toY = 0;
-  const y = useSharedValue(fromY);
+function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>{children}</ScrollView>
+  );
+}
 
-  const fromOpacity = 1;
-  const toOpacity = 0;
-  const opacity = useSharedValue(fromOpacity);
+const AnimatedLetter = ({ text, index }: { text: string; index: number }) => {
+  const duration = 1100;
+  const delayMultiplier = 75;
 
-  const duration = 1000;
+  const y = useSharedValue(1);
+  const opacity = useSharedValue(1);
+  const bottomScale = useSharedValue(0);
 
   const topAnimatedStyles = useAnimatedStyle(() => ({
-    fontSize: 24,
     opacity: opacity.value,
     transform: [{ scaleY: y.value }],
   }));
 
-  useEffect(() => {
-    const reverse = false;
-    y.value = withDelay(
-      index * 100,
-      withRepeat(withSpring(toY, { duration }), -1, reverse)
-    );
-    opacity.value = withDelay(
-      index * 100,
-      withRepeat(withSpring(toOpacity, { duration }), -1, reverse)
-    );
-  }, []);
-
-  const fromBottomY = 20;
-  const toBottomY = 0;
-  const bottomY = useSharedValue(fromBottomY);
-
-  const fromBottomScale = 0;
-  const toBottomScale = 1;
-  const bottomScale = useSharedValue(fromBottomScale);
-
   const bottomAnimatedStyles = useAnimatedStyle(() => ({
-    fontSize: 24,
     position: "absolute",
     transform: [{ scaleY: bottomScale.value }],
   }));
 
-  const topStyle: StyleProp<AnimatedStyle<StyleProp<TextStyle>>> = {
-    transformOrigin: "top",
-  };
-  const bottomStyle: StyleProp<AnimatedStyle<StyleProp<TextStyle>>> = {
-    transformOrigin: "bottom",
-  };
-
   useEffect(() => {
-    const reverse = false;
-    bottomY.value = withDelay(
-      index * 100,
-      withRepeat(withSpring(toBottomY, { duration }), -1, reverse)
+    const delay = index * delayMultiplier;
+
+    y.value = withDelay(delay, withRepeat(withSpring(0, { duration }), -1));
+
+    opacity.value = withDelay(
+      delay,
+      withRepeat(withSpring(0, { duration }), -1)
     );
+
     bottomScale.value = withDelay(
-      index * 100,
-      withRepeat(withSpring(toBottomScale, { duration }), -1, reverse)
+      delay,
+      withRepeat(withSpring(1, { duration, overshootClamping: true }), -1)
     );
-  }, []);
+  }, [index]);
 
   return (
     <Animated.View>
-      <Animated.Text style={[topStyle, topAnimatedStyles]}>
+      <Animated.Text
+        style={[styles.sharedStyle, styles.topStyle, topAnimatedStyles]}
+      >
         {text}
       </Animated.Text>
-      <Animated.Text style={[bottomStyle, bottomAnimatedStyles]}>
+      <Animated.Text
+        style={[styles.sharedStyle, styles.bottomStyle, bottomAnimatedStyles]}
+      >
         {text}
       </Animated.Text>
     </Animated.View>
   );
-}
+};
 
-function SplitMovingText({ text = "hello" }: { text?: string }) {
-  return (
-    <Animated.View style={{ flexDirection: "row" }}>
-      {text.split("").map((char, i) => (
-        <AnimatedLetter key={i} text={char} index={i} />
-      ))}
-    </Animated.View>
-  );
-}
+const SplitMovingText = ({ text }: { text: string }) => (
+  <Animated.View style={{ flexDirection: "row" }}>
+    {text.split("").map((char, i) => (
+      <AnimatedLetter key={i} text={char} index={i} />
+    ))}
+  </Animated.View>
+);
+
+const styles = {
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  topStyle: {
+    transformOrigin: "top",
+  },
+  bottomStyle: {
+    transformOrigin: "bottom",
+  },
+  sharedStyle: {
+    fontSize: 40,
+  },
+} as const;
